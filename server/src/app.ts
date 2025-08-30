bf3e31e83d421cdd2d6e44e26ca370b73556d326
Edinilson <edinilsonsantossilvafilho@outlook.com>
2025-08-29 22:56:15 -0300

    Quinta Alteração

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,14 @@
 import express, { Express, Request, Response } from "express";
 import cors from "cors";
-//import sequelize from "../database/config/db";
-import Users from "./routes/users";
+import PuUsers from "./routes/public";
+import PrUsers from "./routes/private";
+import auth from "../middlewares/auth"
 
 const app: Express = express();
 app.use(express.json());
 app.use(cors({ origin: "http://localhost:5173" }));
 
-app.use("/api", Users);
+app.use("/api", PuUsers);
+app.use("/api", auth, PrUsers);
 
 export default app;
--- server/src/routes/public.ts
+++ server/src/routes/public.ts
@@ -1,11 +1,12 @@
 import { Router, Request, Response } from "express";
+import bcrypt from "bcrypt";
 
 const router = Router();
 
 router.post("/users", (req: Request, res:Response) => {
     const {usuario, senha} = req.body;
 
-    //Aqui vai ficar a parte de receber do FrontEnd e guardar no banco de dados
+    //Aqui vai ficar a parte de receber do FrontEnd, encriptar a senha e guardar no banco de dados
 
 })
 
