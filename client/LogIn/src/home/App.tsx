ccc14eb2fa03b4e5071caaa5b47df4c92a1f6314
Edinilson <edinilsonsantossilvafilho@outlook.com>
2025-09-10 19:23:49 -0300

    Integração do FrontEnd com BackEnd

--- client/LogIn/src/home/App.tsx
+++ client/LogIn/src/home/App.tsx
@@ -3,28 +3,64 @@ import './App.css'
 import { Link } from "react-router-dom"
 
 function App() {
-  const [usuario, setUsuario] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
   const [senha, setSenha] = useState<string>("");
+  const [mensagem, setMensagem] = useState<string>("");
 
-  function Entrar(event: React.FormEvent<HTMLFormElement>) {
+  type token = {
+    token: string;
+  }
+
+
+  async function Entrar(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
-    //A adicionar integração com BackEnd
+    try {
+      
+      const resposta = await fetch("http://localhost:3000/api/login", {
+        method: "post",
 
-  }
+        headers: {
+          "Content-Type": "application/json"
+
+        },
+
+        body: JSON.stringify({email, password: senha})
+
+      });
+
+      if (!resposta.ok) {
+
+        const err: any = await resposta.json();
+        throw new Error(err.message || "Erro no servidor");
+
+      } else {
+        const dados: token = await resposta.json();
+
+        setMensagem(`Logado com token ${dados.token}`);
+
+      }
+
+
+    } catch (err: any) {
+      setMensagem("Erro: " + err.message)
+    }
+
+  };
 
   return (
     <>
         <div className='div1'>
           <h1>LogIn</h1>
           <form onSubmit={Entrar}>
-            <label htmlFor="">Nome de Usuário:</label>
-            <input type="text" value={usuario} placeholder="Ex: Gustavo Pistori Faraço" onChange={(e) => setUsuario(e.target.value)}/>
+            <label htmlFor="">Email:</label>
+            <input type="text" value={email} onChange={(e) => setEmail(e.target.value)}/>
             <label htmlFor="">Senha:</label>
             <input type="password" value={senha} onChange={(e) => setSenha(e.target.value)}/>
             <input type="submit" value="Entrar"/>
           </form>
           <p>Caso não seja cadastrado, <Link to="/registro" className='link'>cadastre-se</Link></p>
+          <span>{mensagem}</span>
         </div>
     </>
   )
--- client/LogIn/src/home/pages/registro.tsx
+++ client/LogIn/src/home/pages/registro.tsx
@@ -1,13 +1,37 @@
-//import { useState } from 'react'
+import { useState } from 'react'
 import '../App.css'
-//import { Link } from "react-router-dom"
+import { Link } from "react-router-dom"
+import "../App.css"
 
 function Registro() {
+  const [senha, setSenha] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [usuario, setUsuario] = useState<string>("");
+  const [mensagem, setMensagem] = useState<string>("");
+
+  async function Registrar(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    //Fazer integração com o BackEnd
+  }
 
 
   return (
     <>
-    {/* Terminar a tela de registro */}
+        <div className='div1'>
+          <h1>Registro</h1>
+          <form onSubmit={Registrar}>
+            <label htmlFor="">Nome:</label>
+            <input type="text" value={usuario} onChange={(e) => setUsuario(e.target.value)}/>
+            <label htmlFor="">Email:</label>
+            <input type="text" value={email} onChange={(e) => setEmail(e.target.value)}/>
+            <label htmlFor="">Senha:</label>
+            <input type="password" value={senha} onChange={(e) => setSenha(e.target.value)}/>
+            <input type="submit" value="Entrar"/>
+          </form>
+          <p>Caso ja tenha uma conta, faça <Link to="/" className='link'>Log-In</Link></p>
+          <span>{mensagem}</span>
+        </div>
     </>
   )
 }
